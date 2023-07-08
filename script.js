c1cc15b6ae4595cce8ae2b029d10264f4816a40e
Yorrane Rhaira <132151850+YorraneRSA@users.noreply.github.com>
2023-07-08 14:43:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let ano_atual = 2023
+let nome = (prompt("Digite o seu nome"))
+let ano_usu = parseFloat(prompt("Digite o ano de nascimento"))
+let idade = 0
+
+alert("Seu nome é " + nome)
+alert("Sua idade é " + (idade=ano_atual-ano_usu))
